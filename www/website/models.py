b2b50a98c826caf2a8020712c2c6afb22cd397a9
Payam Mohammadi <payam.int@gmail.com>
2018-03-09 10:07:29 +0330

    Change `.gitignore`

--- www/website/models.py
+++ www/website/models.py
@@ -1,4 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Field(models.Model):
+    title = models.CharField(max_length=255)
+
+class UserProfile(models.Model):
+    student_id = models.IntegerField(max_length=9)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+
 
