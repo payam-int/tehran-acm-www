9b9497598eeecd804cf8ab265e4e16354ab56d9d
Payam Mohammadi <payam.int@gmail.com>
2018-03-09 10:52:00 +0330

    Initial commit

--- www/account/form.py
+++ www/account/form.py
@@ -4,22 +4,19 @@ from django import forms
 from django.contrib.auth.models import User
 
 from account.models import StudentField
-from account.validators import validate_student_id
+from account.validators import validate_student_id, email_validator
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required', required=True)
+    email = forms.EmailField(max_length=200, help_text='Required', required=True, validators=[email_validator])
     field = forms.ModelChoiceField(queryset=StudentField.objects.all())
     student_id = forms.IntegerField(validators=[validate_student_id])
 
     def validate(self):
         email = self.cleaned_data['email']
         username = self.cleaned_data['username']
-        if User.objects.filter(email=email).exclude(username=username).exists() or re.match(
-                r'^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@ut.ac.ir$', email):
+        if User.objects.filter(email=email).exclude(username=username).exists():
             raise forms.ValidationError(u'Invalid data.')
-
-
 
     class Meta(UserCreationForm.Meta):
         model = User
--- www/account/validators.py
+++ www/account/validators.py
@@ -5,7 +5,13 @@ from django.utils.translation import gettext_lazy as _
 
 
 def validate_student_id(value):
-    if not re.match(r'^\d{9}$', value.strip()):
+    if not re.match(r'^\d{9}$', str(value).strip()):
         raise ValidationError(
             _('The student id has exactly 9 digits'),
+        )
+def email_validator(value):
+    if not re.match(
+                r'^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@ut.ac.ir$', value):
+        raise ValidationError(
+            _('This email does not belong to ut.'),
         )
